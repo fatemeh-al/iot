77c8db25b4aa7c42a2bcd64777eaf10728d9c1bb
fatemeh-al <46268046+fatemeh-al@users.noreply.github.com>
2019-01-01 00:05:33 +0330

    Update signup.js

--- signup.js
+++ signup.js
@@ -1,15 +1,24 @@
 var btnObject = document.getElementById('btn');
-var inpObject = document.getElementById("inp");
-var singupbtn = document.getElementById("singupbtn");
+var inpObject = document.getElementById('inp');
+var signupbtn = document.getElementById('signupbtn');
 
 btnObject.addEventListener("click",function(event){
-    inp.innerHTML += '<div class="dont"><label for="MemberName">Member Name</label><input type="text" name="MemberName" required><br></div>';
+    inp.innerHTML += '<div class="dont"><label for="MemberName">Member Name</label><input type="text" name="MemberName"  id="memberName" required><br></div>';
 })
 
-singupbtn.addEventListener("click", function(event){
+signupbtn.addEventListener("click", function(event){
+	var groupName = document.getElementById('groupName');
+	var ThingSpeakID = document.getElementById('ThingSpeakID');
+	var password = document.getElementById('password');
+	var memberName = document.getElementById('memberName');
+
 	request = new XMLHttpRequest();
-	request.open('POST', 'URL');
-	request.send(JSON.strigify({
-		//get username o ina
+	request.open('POST', 'https://iot2256.herokuapp.com/signup');
+	request.setRequestHeader('Content-Type', 'application/json');
+	request.send(JSON.stringify({
+		groupName:groupName,
+		ThingSpeakID:ThingSpeakID,
+		password:password,
+		memberName:memberName
 	}));
 })
