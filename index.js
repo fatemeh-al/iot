495b2b7a744a84f01514fd5b5569b4d2af39ecea
fatemeh-al <46268046+fatemeh-al@users.noreply.github.com>
2019-01-01 00:04:20 +0330

    Update index.js

--- index.js
+++ index.js
@@ -16,21 +16,37 @@
 //   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
 
-var express = require('express');
-var app = express();
-var bodyparser = require('body-parser');
-var cors = require('cors');
+var express = require('express')
+var app = express()
+var bodyparser = require('body-parser')
+var cors = require('cors')
 const PORT = process.env.PORT || 3000
+const path = require('path')
 
-app.use(bodyparser.json()); 
+app.use(bodyparser.json())
+app.use(cors())
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
 app.get('/', function (req, res) {
-  res.send('Hello World');
-})
+  res.send('Hello World')
+});
 
 app.get('/test', function(req, res){
-	res.send('tesssssst');
-})
- 
-app.listen(PORT)
+	res.send('tesssssst')
+});
 
+app.post('/signup', function(req, res){
+	res.setHeader('Content-Type', 'application/json');
+	console.log("success");
+	res.send(`someone logged in`);
+});
+ 
+app.listen(PORT);
